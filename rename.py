fda8656b3b9736890e67418e166d761a070dcb12
Moon Ju Hyeon <46799820+JMacoustic@users.noreply.github.com>
2025-03-23 03:24:23 +0900

    Add files via upload

---
+++ rename.py
@@ -0,0 +1,22 @@
+import os
+
+start_old = 1
+end_old = 460
+start_new = 650
+
+for i in range(start_old, end_old + 1):
+    old_name = f"data_decay_{i:04d}.mp4"
+    new_index = start_new + (i - start_old)
+    new_name = f"data_decay_{new_index:04d}.mp4"
+
+    if os.path.isdir(old_name):
+        os.rename(old_name, new_name)
+
+
+for i in range(start_old, end_old + 1):
+    old_name = f"config_decay_{i:04d}.json"
+    new_index = start_new + (i - start_old)
+    new_name = f"config_decay_{new_index:04d}.json"
+
+    if os.path.isdir(old_name):
+        os.rename(old_name, new_name)
